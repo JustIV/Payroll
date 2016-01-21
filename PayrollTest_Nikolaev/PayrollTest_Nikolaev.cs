6d934fa51300004f2a5f11972b9c0f8d6a65b2ad
JustIV <Just_IV@bk.ru>
2016-01-21 20:47:57 +0400

    first

---
+++ PayrollTest_Nikolaev/PayrollTest_Nikolaev.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Payroll_Nikolaev;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PayrollTest_Nikolaev
+{
+    [TestClass]
+    public class PayrollTest_Nikolaev
+    {
+        [TestMethod]
+        public void TestEmployee()
+        {
+            int empId = 1;
+            Employee e = new Employee(empId, "Bob", "Home");
+            Assert.AreEqual("Bob", e.Name);
+            Assert.AreEqual("Home", e.Address);
+            Assert.AreEqual(empId, e.EmpId);  
+        }
+
+        [TestMethod]
+        public void TestEmployeeToString()
+        {
+            int empId = 1;
+            Employee e = new Employee(empId, "TestEmployee", "TestAddress");
+            e.ToString();
+        }
+
+        [TestMethod]
+        public void TestAddSalariedEmployee()
+        {
+            int empId = 1;
+            AddSalariedEmployee t = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+            t.Execute();
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            Assert.AreEqual("Bob", e.Name);
+            PaymentClassification pc = e.Classification;
+            Assert.IsTrue(pc is SalariedClassification);
+            SalariedClassification sc = pc as SalariedClassification;
+            Assert.AreEqual(1000.00, sc.Salary, .001);
+            PaymentSchedule ps = e.Schedule;
+            Assert.IsTrue(ps is MonthlySchedule);
+            PaymentMethod pm = e.Method;
+            Assert.IsTrue(pm is HoldMethod);
+        }
+        [TestMethod]
+        public void TestAddHourlyEmployee()
+        {
+            int empId = 1;
+            AddHourlyEmployee t = new AddHourlyEmployee(empId, "Bob", "Home", 100.00);
+            t.Execute();
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            Assert.AreEqual("Bob", e.Name);
+            PaymentClassification pc = e.Classification;
+            Assert.IsTrue(pc is HourlyClassification);
+            HourlyClassification sc = pc as HourlyClassification;
+            Assert.AreEqual(100, sc.HourlyRate, 0.001);
+            PaymentSchedule ps = e.Schedule;
+            Assert.IsTrue(ps is WeeklySchedule);
+            PaymentMethod pm = e.Method;
+            Assert.IsTrue(pm is HoldMethod);
+        }
+
+        [TestMethod]
+        public void TestAddCommissionedEmployee()
+        {
+            int empId = 1;
+            AddCommissionedEmployee t = new AddCommissionedEmployee(empId, "Bob", "Home", 100.00, 50.00);
+            t.Execute();
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            Assert.AreEqual("Bob", e.Name);
+            PaymentClassification pc = e.Classification;
+            Assert.IsTrue(pc is CommissionedClassification);
+            CommissionedClassification sc = pc as CommissionedClassification;
+            Assert.AreEqual(100.00, sc.Salary, .001);
+            Assert.AreEqual(50.00, sc.CommissionRate, .001);
+            PaymentSchedule ps = e.Schedule;
+            Assert.IsTrue(ps is BiweeklySchedule);
+            PaymentMethod pm = e.Method;
+            Assert.IsTrue(pm is HoldMethod);
+        }
+    }
+}
---
+++ Payroll_Nikolaev/CommissionedClassification.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_Nikolaev
+{
+    public class CommissionedClassification : PaymentClassification
+    {
+        private readonly double salary;
+        private readonly double commissionRate;
+
+        public CommissionedClassification(double salary, double commissionRate)
+        {
+            this.salary = salary;
+            this.commissionRate = commissionRate;
+        }
+
+        public double CommissionRate
+        {
+            get { return commissionRate; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("${0} ${1}", commissionRate, salary);
+        }
+    }
+}
